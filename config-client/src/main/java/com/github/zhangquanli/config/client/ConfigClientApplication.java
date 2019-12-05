788aafae699eb1349c9c7e6c54fcda036b5b8c6e
zhangquanli <502624709@qq.com>
2019-12-05 12:00:54 +0800

    首次提交

--- config-client/src/main/java/com/github/zhangquanli/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/github/zhangquanli/config/client/ConfigClientApplication.java
@@ -2,8 +2,12 @@ package com.github.zhangquanli.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
+@EnableDiscoveryClient
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
